9bff1768e07056f279a36b7ddadb859a9b360c32
daniil-kiryanov <daniil.kiryanov@gmail.com>
2022-09-24 20:55:56 +0400

    Add project files.

---
+++ Snake_v1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics;
+using static System.Console;
+namespace Snake_v1;
+class Program
+{
+    private const int MapWidth = 30;
+    private const int MapHeight = 20;
+    private const int ScreenWidth = MapWidth * 3;
+    private const int ScreenHeight = MapHeight * 3;
+    private const ConsoleColor BorderColor = ConsoleColor.Gray;
+    private const ConsoleColor HeadColor = ConsoleColor.Red;
+    private const ConsoleColor BodyColor = ConsoleColor.Green;
+    private const int FrameMs = 200;
+    static void Main()
+    {
+        SetWindowSize(ScreenWidth, ScreenHeight);
+        SetBufferSize(ScreenWidth, ScreenHeight);
+        CursorVisible = false;
+        DrawBorder();
+        Direction currentMovement = Direction.Right;
+        var snake = new Snake(10, 5, HeadColor, BodyColor);
+        Stopwatch sw = new Stopwatch();
+        while (true)
+        {
+            sw.Restart();
+            Direction oldMovement = currentMovement;
+            while (sw.ElapsedMilliseconds <= FrameMs)
+            {
+                if (currentMovement == oldMovement)
+                {
+                    currentMovement = ReadMovement(currentMovement);
+                }
+            }
+            snake.Move(currentMovement);
+            if (snake.Head.X == MapWidth - 1
+                || snake.Head.X == 0
+                || snake.Head.Y == MapHeight - 1
+                || snake.Head.Y == 0
+                || snake.Body.Any(b => b.X == snake.Head.X && b.Y == snake.Head.Y))
+                break;
+        }
+        snake.Clear();
+        SetCursorPosition(ScreenWidth / 3, ScreenHeight / 2);
+        WriteLine("GameOver");
+        ReadKey();
+    }
+    static Direction ReadMovement(Direction currentDirection)
+    {
+        if (!KeyAvailable)
+            return currentDirection;
+        ConsoleKey key = ReadKey(true).Key;
+        currentDirection = key switch
+        {
+            ConsoleKey.UpArrow when currentDirection != Direction.Down => Direction.Up,
+            ConsoleKey.DownArrow when currentDirection != Direction.Up => Direction.Down,
+            ConsoleKey.LeftArrow when currentDirection != Direction.Right => Direction.Left,
+            ConsoleKey.RightArrow when currentDirection != Direction.Left => Direction.Right,
+            _ => currentDirection
+        };
+        return currentDirection;
+    }
+
+    static void DrawBorder()
+    {
+        for (int i = 0; i < MapWidth; i++) //horizontal border
+        {
+            new Pixel(i, 0, BorderColor).Draw();
+            new Pixel(i, MapHeight - 1, BorderColor).Draw();
+        }
+        for (int i = 0; i < MapHeight; i++) //vertical border
+        {
+            new Pixel(0, i, BorderColor).Draw();
+            new Pixel(MapWidth - 1, i, BorderColor).Draw();
+        }
+    }
+
+}
---
+++ Snake_v1/Snake.cs
@@ -0,0 +1,62 @@
+﻿namespace Snake_v1
+{
+    public class Snake
+    {
+        private readonly ConsoleColor headColor;
+        private readonly ConsoleColor bodyColor;
+
+        public Pixel Head { get; private set; }
+
+        public Snake(int initialX, int initialY, ConsoleColor headColor, ConsoleColor bodyColor, int bodyLenght = 3)
+        {
+            this.headColor = headColor;
+            this.bodyColor = bodyColor;
+
+            Head = new Pixel(initialX, initialY, headColor);
+
+            for (int i = bodyLenght; i >= 0; i--)
+            {
+                Body.Enqueue(new Pixel(Head.X - i - 1, initialY, bodyColor));
+            }
+            Draw();
+
+        }
+
+        public Queue<Pixel> Body { get; } = new Queue<Pixel>();
+
+        public void Move(Direction direction)
+        {
+            Clear();
+            Body.Enqueue(new Pixel(Head.X, Head.Y, bodyColor));
+            Body.Dequeue();
+            Head = direction switch
+            {
+                Direction.Right => new Pixel(Head.X + 1, Head.Y, headColor),
+                Direction.Left => new Pixel(Head.X - 1, Head.Y, headColor),
+                Direction.Up => new Pixel(Head.X, Head.Y - 1, headColor),
+                Direction.Down => new Pixel(Head.X, Head.Y + 1, headColor),
+                _ => Head
+            };
+            Draw();
+        }
+
+        public void Draw()
+        {
+            Head.Draw();
+            foreach (Pixel pixel in Body)
+            {
+                pixel.Draw();
+            }
+        }
+
+        public void Clear()
+        {
+            Head.Clear();
+            foreach (Pixel pixel in Body)
+            {
+                pixel.Clear();
+            }
+        }
+
+    }
+}
